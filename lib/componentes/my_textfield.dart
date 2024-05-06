18152a9b3ae38e0e8d52e7fc4066a6b76183cf6b
victormlaker <victorhugomlaker9@gmail.com>
2024-05-05 22:14:42 -0300

    Construindo a tela e adicionando campos de usuário e senha

---
+++ lib/componentes/my_textfield.dart
@@ -0,0 +1,26 @@
+import "package:flutter/material.dart";
+
+class MyTextField extends StatefulWidget {
+  const MyTextField({super.key});
+
+  @override
+  State<MyTextField> createState() => _MyTextFieldState();
+}
+
+class _MyTextFieldState extends State<MyTextField> {
+  @override
+  Widget build(BuildContext context) {
+    return Padding(
+      padding: const EdgeInsets.symmetric(horizontal: 25.0),
+      child: TextField(
+        decoration: InputDecoration(
+            enabledBorder:
+                OutlineInputBorder(borderSide: BorderSide(color: Colors.white)),
+            focusedBorder: OutlineInputBorder(
+                borderSide: BorderSide(color: Colors.grey.shade400)),
+            fillColor: Colors.grey.shade200,
+            filled: true),
+      ),
+    );
+  }
+}
--- lib/views/login.dart
+++ lib/views/login.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:prisma/componentes/my_textfield.dart';
 
 class Login extends StatefulWidget {
   const Login({super.key});
@@ -11,9 +12,33 @@ class _LoginState extends State<Login> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(
-        backgroundColor: Colors.blue,
+      backgroundColor: Colors.white,
+      body: SafeArea(
+        child: Center(
+          child: Column(
+          children: [
+            const SizedBox(height: 50),
+
+
+            // Logo bala 
+            Icon(
+              Icons.lock
+              size: 100,
+            ), 
+
+            const SizedBox(height: 25),
+
+            // Componente de Usuário
+            MyTextField(),
+
+            const SizedBox(height: 10),
+
+            // Componente de Senha
+            MyTextField(),            
+          ],
+          ),
       ),
-    );
+    )
+  );
   }
 }
