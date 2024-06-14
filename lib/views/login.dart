c19ce04214fe08bad472cb1a31ab797d48671304
Victor e Pedro <victorhugomlaker9@gmail.com>
2024-06-13 21:13:20 -0300

    Adicionando a opção de ocultar a senha

--- lib/views/login.dart
+++ lib/views/login.dart
@@ -14,32 +14,29 @@ class Login extends StatefulWidget {
 }
 
 class _LoginState extends State<Login> {
+  bool _showPassword = false;
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      backgroundColor: Colors.white,
-     
-      drawer: const hamburguer(),
-
-      appBar: AppBar(
-
-        toolbarHeight: 100,
-        
-        backgroundColor: Color(0xFF7D95FF),
-
-        title: Image.asset("img/logo.jpg"),  
- 
-        actions:[
-
-        IconButton(
-          icon: const Icon(Icons.search),
-          onPressed: (){
-            showSearch(context: context, delegate: MySearchDelegate(),
-            );
-          },
+        backgroundColor: Color.fromRGBO(255, 254, 241, 1.000),
+        drawer: const hamburguer(),
+        appBar: AppBar(
+          toolbarHeight: 100,
+          backgroundColor: Color(0xFF7D95FF),
+          title: Image.asset("img/logo.jpg"),
+          actions: [
+            IconButton(
+              icon: const Icon(Icons.search),
+              onPressed: () {
+                showSearch(
+                  context: context,
+                  delegate: MySearchDelegate(),
+                );
+              },
+            ),
+          ],
         ),
-      ],
-    ),
         body: SafeArea(
           child: Center(
             child: Column(
@@ -64,15 +61,29 @@ class _LoginState extends State<Login> {
 
                 // Componente de Senha
                 TextField(
-                    decoration: InputDecoration(
-                        enabledBorder: OutlineInputBorder(
-                            borderSide: BorderSide(color: Colors.white)),
-                        focusedBorder: OutlineInputBorder(
-                            borderSide:
-                                BorderSide(color: Colors.grey.shade400)),
-                        fillColor: Colors.grey.shade200,
-                        filled: true,
-                        hintText: "Senha do usuário")),
+                  decoration: InputDecoration(
+                      enabledBorder: OutlineInputBorder(
+                          borderSide: BorderSide(color: Colors.white)),
+                      focusedBorder: OutlineInputBorder(
+                          borderSide: BorderSide(color: Colors.grey.shade400)),
+                      fillColor: Colors.grey.shade200,
+                      filled: true,
+                      suffixIcon: GestureDetector(
+                        child: Icon(
+                          _showPassword == false
+                              ? Icons.visibility_off
+                              : Icons.visibility,
+                          color: Colors.black,
+                        ),
+                        onTap: () {
+                          setState(() {
+                            _showPassword = !_showPassword;
+                          });
+                        },
+                      ),
+                      hintText: "Senha do usuário"),
+                  obscureText: _showPassword == false ? true : false,
+                ),
 
                 const SizedBox(height: 10),
 
